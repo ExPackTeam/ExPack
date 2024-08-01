22b56bcfa4b938223bb9cc27d02551e7e31cde4a
MicroRay620 <microray620@gmail.com>
2024-08-01 12:21:55 -0400

    fixed error with bg-[color] js error

--- src/js/global.js
+++ src/js/global.js
@@ -1,16 +1,26 @@
-// Add code to auto detect hex code in an attribute
-var elements = document.querySelectorAll("[class*='bg-']");
-var data_elements = getAttribute(attributeName);
-elements.addEventListener("DOMContentLoaded", () => {
-    elements.forEach(elements => {
-        const classes = elements.className.split(" ");
-        classes.forEach(cl => {
-            if (cl.startsWith("bg-")) {
-                const colorCode = cls.substring(3);
-                if (/^[0-9A-Fa-f]{6}$/.test(colorCode)) {
-                    element.style.backgroundColor = `#${colorCode}`;
-                };
-            };
+document.addEventListener("DOMContentLoaded", () => {
+    const bg_elements = document.querySelectorAll("[class*='bg-']");
+    bg_elements.forEach(element => {
+      const classes = element.className.split(" ");
+      classes.forEach(cls => {
+        if (cls.startsWith("bg-")) {
+          const colorCode = cls.substring(3);
+          if (/^[0-9A-Fa-f]{6}$/.test(colorCode)) {
+            element.style.backgroundColor = `#${colorCode}`;
+          }
+        }
+      });
+    });
+    const mn_elements = document.querySelectorAll("[class*='color-']");
+    mn_elements.forEach(element => {
+        const classes = element.className.split(" ");
+        classes.forEach(cls => {
+          if (cls.startsWith("color-")) {
+            const colorCode = cls.substring(3);
+            if (/^[0-9A-Fa-f]{6}$/.test(colorCode)) {
+              element.style.backgroundColor = `#${colorCode}`;
+            }
+          }
         });
     });
 });
