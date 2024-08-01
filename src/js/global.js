3387963e60dc87744316ed52b27757f2d712541b
MicroRay620 <microray620@gmail.com>
2024-08-01 12:13:01 -0400

    added debug.html

--- src/js/global.js
+++ src/js/global.js
@@ -1,1 +1,16 @@
 // Add code to auto detect hex code in an attribute
+var elements = document.querySelectorAll("[class*='bg-']");
+var data_elements = getAttribute(attributeName);
+elements.addEventListener("DOMContentLoaded", () => {
+    elements.forEach(elements => {
+        const classes = elements.className.split(" ");
+        classes.forEach(cl => {
+            if (cl.startsWith("bg-")) {
+                const colorCode = cls.substring(3);
+                if (/^[0-9A-Fa-f]{6}$/.test(colorCode)) {
+                    element.style.backgroundColor = `#${colorCode}`;
+                };
+            };
+        });
+    });
+});
