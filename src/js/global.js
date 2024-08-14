7aceb6947f171a4fc0972cd5dd0ee482b4f22d0c
MicroRay620 <microray620@gmail.com>
2024-08-14 13:11:40 -0400

    created

--- src/js/global.js
+++ src/js/global.js
@@ -1,27 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const bg_elements = document.querySelectorAll("[class*='bg-']");
-    bg_elements.forEach(element => {
+  const bgElements = document.querySelectorAll("[class*='bg-']");
+  bgElements.forEach(element => {
+    const classes = element.className.split(" ");
+    classes.forEach(cls => {
+      if (cls.startsWith("bg-")) {
+        const colorCode = cls.substring(3);
+        if (/^[0-9A-Fa-f]{6}$/.test(colorCode)) {
+          element.style.backgroundColor = `#${colorCode}`;
+        }
+      }
+    });
+  });
+  /* Issue where it won't change the color of the text with the class='color' */
+  const mnElements = document.querySelectorAll("[class*='fr-']");
+  mnElements.forEach(element => {
       const classes = element.className.split(" ");
       classes.forEach(cls => {
-        if (cls.startsWith("bg-")) {
+        if (cls.startsWith("color-")) {
           const colorCode = cls.substring(3);
           if (/^[0-9A-Fa-f]{6}$/.test(colorCode)) {
-            element.style.backgroundColor = `#${colorCode}`;
+            element.style.color = `#${colorCode}`;
           }
         }
       });
-    });
-    /* Issue where it won't change the color of the text with the class='color' 
-    const mn_elements = document.querySelectorAll("[class*='color-']");
-    mn_elements.forEach(element => {
-        const classes = element.className.split(" ");
-        classes.forEach(cls => {
-          if (cls.startsWith("color-")) {
-            const colorCode = cls.substring(3);
-            if (/^[0-9A-Fa-f]{6}$/.test(colorCode)) {
-              element.style.color = `#${colorCode}`;
-            }
-          }
-        });
-    }); */
+  }); //*/
 });
