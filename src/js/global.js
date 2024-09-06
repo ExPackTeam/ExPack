d095ed90e545d1394b1a7f2470b5ede7d600e88f
ruby_rose <46199301+MicroRay620@users.noreply.github.com>
2024-09-06 12:00:56 -0400

    modified, no bug fixes

--- src/js/global.js
+++ src/js/global.js
@@ -1,6 +1,44 @@
 import "modals";
-import "text_changes";
+import { modalFunction } from "text_changes";
+modalFunction();
 document.addEventListener("DOMContentLoaded", () => { 
+	const bgElements = document.querySelectorAll("[class*='bg-']");
+	bgElements.forEach(element => {
+		const classes = element.className.split(" ");
+		classes.forEach(cls => {
+			if (cls.startsWith("bg-")) {
+				const colorCode = cls.substring(3);
+				if (/^[0-9A-Fa-f]{6}$/.test(colorCode)) {
+					element.style.backgroundColor = `#${colorCode}`;
+				}
+			}
+		});
+	});
+	const mnElements = document.querySelectorAll("[class*='fr-']");
+	mnElements.forEach(element => {
+		const classes = element.className.split(" ");
+		classes.forEach(cls => {
+			if (cls.startsWith("fr-")) {
+				const colorCode = cls.substring(3);
+				if (/^[0-9A-Fa-f]{6}$/.test(colorCode)) {
+					element.style.color = `#${colorCode}`;
+				}
+			}
+		});
+	});
+    // FINALLY FUCKING WORKS!!!!!!! 15/08/2024
+    const fontWeightClass = document.querySelectorAll("[class^='fw-'");
+	fontWeightClass.forEach(element => {
+		const classes = element.className.split(" ");
+		classes.forEach(cls => {
+			if (cls.startsWith("fw-")) {
+				const fwValue = cls.substring(3);
+				if (/^\d+px?$/.test(fwValue)) {
+					element.style.fontWeight = fwValue;
+				}
+			}
+		});
+	});
 	const closingClass = document.getElementByClassName("close");
 	closingClass.textContent = "&times;";
 	const elementAlignment = document.querySelectorAll("[class^='align-']");
--- src/js/modals.js
+++ src/js/modals.js
@@ -1,15 +1,17 @@
 // modified from w3schools
-var modal = document.getElementsByClassName("modal");
-var modalBtn = document.getElementsByClassName("modal-btn");
-var closeClick = document.getElementsByClassName("close");
-modalBtn.onclick = function() {
-  modal.style.display = "block";
-}
-closeClick.onclick = function() {
-  modal.style.display = "none";
-}
-window.onclick = function(event) {
-  if (event.target == modal) {
+function modalFunction() {
+  var modal = document.getElementsByClassName("modal");
+  var modalBtn = document.getElementsByClassName("modal-btn");
+  var closeClick = document.getElementsByClassName("close");
+  modalBtn.onclick = function() {
+    modal.style.display = "block";
+  }
+  closeClick.onclick = function() {
     modal.style.display = "none";
+  }
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
   }
 }
