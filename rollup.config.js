8ec2cbae94e7baf1c76d6ed175978e89c983856f
ruby_rose <46199301+MicroRay620@users.noreply.github.com>
2024-09-24 11:53:16 -0400

    modified, changed some of the formatting

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,8 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import { babel } from "@rollup/plugin-babel";
-import { terser } from "rollup-plugin-terser";
-import postcss from 'rollup-plugin-postcss';; 
+import terser from "@rollup/plugin-terser";
+import postcss from 'rollup-plugin-postcss';
 import postcssAttributeCaseInsensitive from "postcss-attribute-case-insensitive";
 const basePlugin = {
   plugins: [
