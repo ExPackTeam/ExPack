977af13bfe3bddea44eff939b9d7784ffeea22d3
ruby_rose <46199301+MicroRay620@users.noreply.github.com>
2025-01-14 20:51:27 -0500

    modified, ready for release

--- src/js/text_changes.js
+++ src/js/text_changes.js
@@ -1,170 +1,120 @@
 import $ from "jquery";
 import { TextBackup } from "./backup";
+// Retype Count: 9 // This is the amount I have re-written this code FROM scratch
+// All `console.log()`s are for debugging, they are going to removed once it's ready for the master branch
 /*
-    All // console.log()s are temporary and only here for debugging.
-    In the master branch, these won't be here
+    This code is completed for now.
+    Will need to figure out how to make it so the code will immediately change to the backup if it fails
 */
-function ColorChanger(className, cssName) {
-    // var classBack = $(this).attr("class", className);
-    var classGet = $(this).attr("class"); // removed attr("class", className);
-    // var classGet = $(this).hasClass(className);
-    var classCall = classGet != undefined ? true : false; // This is here to let there be a boolean for className and className stays a string
-    /*  
-        retrying with $(this) instead of $("*")
-    */
-    console.log("Class Get is " + classGet + /*"\nClass Back is " + classBack +*/ "\nClass Call is " + classCall);
-    var backUp = classCall == false  ? true : false;
-    console.log("Started Color Changing");
-    if (backUp == false) {
-        console.log("Class Call is " + classCall);
-        if (classCall == true) {
-            console.log("Class Get is " + classGet);
-            $(classGet).each(function() {
-                var regex = /^[0-9A-Fa-f]{6}$/
-                var match = classGet.match(regex);
-                console.log("regex is " + regex);
-                console.log("match is " + match);
-                if (match) {
-                    var hexCode = match[0];
-                    if (hexCode) {
-                        if (/^[0-9A-Fa-f]{6$/.text(hexCode)) {
-                            $(this).css(cssName, hexCode);
+function TextOptions(cssName) {
+    var classGet = [];
+    var backUp = null;
+    const classLook = $("body").find("[class]").each(function () {
+        var classSearch = $(this).attr("class");
+        const getBackUp = classSearch == undefined ? true : false;
+        classGet = classSearch ? classSearch.split(" ") : null;
+        backUp = getBackUp;
+        // In testing: put a console.log() check for classGet here, it involves the classGet variable
+        // In testing: put a console.log() check for backUp here, it involves the backUp variable
+        if (backUp != true) {
+            // In testing: put a console.log() check for backUp being false here, it doesn't involve variables
+            // In testing: put a console.log() check for classGet here, it involves the classGet variable
+            const classGetSize = classGet.length;
+            // In testing: put a console.log() check for classGetSize here, it involves the classGetSize variable
+            if (classGetSize == 1) {
+                // In testing: put a console.log() check for classGetSize being 1 here, it doesn't involve variables
+                var classPlace = classGet[0];
+                // In testing: put a console.log() check for classPlace here, it involves the classPlace variable
+                if (classPlace) {
+                    if (classPlace.includes("bg") || classPlace.includes("fg")) {
+                        var classPart = classPlace.substring(3);
+                        // In testing: put a console.log() check for classPart here, it involves the classPart variable
+                        var color;
+                        if (/^[0-9A-Fa-f]{6}$/.test(classPart)) {  
+                             color = `#${classPart}`;
+                        } else {
+                            color = classPart; 
                         }
-                    } else {
-                        console.error("[hexCode] is " + hexCode);
-                    }
-                } else {
-                    console.error("[match] is " + match);
-                }
-            });
-        } /* else {
-            console.log("Class Back is " + classBack);
-            $(classBack).each(function() {
-                var regex = /^[0-9A-Fa-f]{6}$/
-                var match = classGet.match(regex);
-                if (match) {
-                    var hexCode = match[0];
-                    if (hexCode) {
-                        if (/^[0-9A-Fa-f]{6$/.text(hexCode)) {
-                            $(this).css(cssName, hexCode);
+                        // In testing: put a console.log() check for color here, it involves the color variable
+                        if (classPlace.includes("bg")) {
+                            $(this).css("background-color", color);
+                        } else if (classPlace.includes("fg")) {
+                            $(this).css("color", color);
                         }
-                    } else {
-                        console.error("[hexCode] is " + hexCode);
+                    } else if (classPlace.includes("fs")) {
+                        var classPart = classPlace.substring(3);
+                        // In testing: put a console.log() check for classPart here, it involves the classPart variable
+                        $(this).css("font-size", classPart);
+                    } else if (classPlace.includes("fw")) {
+                        var classPart = classPlace.substring(3);
+                        // In testing: put a console.log() check for classPart here, it involves the classPart variable
+                        $(this).css("font-weight", classPart);
                     }
-                } else {
-                    console.error("[match] is " + match);
                 }
-            });
-        } */
-    } else if (backUp == true) {
-        TextBackup();
-        var jqueryScriptFail = new Error("Indended color changing jQuery code failed");
-        var undefinedError;
-        if (classGet === undefined) {
-            undefinedError = new Error("[classGet] in {ColorChanger} is undefined");
-        } else if (className === undefined) {
-            undefinedError = new Error("[className] in {ColorChanger} is undefined");
-        }
-        if (this != undefined && className != undefined) {
-            console.error(jqueryScriptFail);
-        } else if (this === undefined || className === undefined) {
-            console.error(jqueryScriptFail + " " + undefinedError);
-        }
-    }
-}
-function NumberChanger(className, cssName) {
-    var classBack = $(this).attr("class", className);
-    var classGet = $(this).attr("class",className);
-    var classCall = $(this).attr("class", className) ? true : false;
-    var backUp = classCall == false ? true : false;
-    console.log("Starting Numbers");
-    console.log("Class Call is " + classCall);
-    console.log("Class Get is " + classGet);
-    console.log("Backup is " + backUp);
-    if (backUp == false) {
-        console.log("Class Call is " + classCall);
-        if (classCall == true) {
-            console.log("Class Get is " + classGet);
-            $(classGet).each(function() {
-                var regex = /^\d+$/;
-                var amount = classGet.match(regex);
-                console.log("Amount is " + amount);
-                if (!isNaN(amount)) {
-                    var fontWeight = amount[0];
-                    console.log("font weight is " + fontWeight);
-                    if (/^\d+$/.test(fontWeight)) {
-                        console.log("Font weight test is successful");
-                        $(this).css(cssName, fontWeight);
-                    } else {
-                        console.error("[fontWeight] returned as " + fontWeight)
+            } else if (classGetSize > 1) {
+                for (var i = 0; i < classGetSize; i++) {
+                    // In testing: put a console.log() check for i here, it involves the i variable
+                    var classPlace = classGet[i];
+                    // In testing: put a console.log() check for classPlace here, it involves the classPlace variable
+                    if (classPlace.includes("bg") || classPlace.includes("fg")) {
+                        var classPart = classPlace.substring(3);
+                        // In testing: put a console.log() check for classPart here, it involves the classPart variable
+                        var color;
+                        if (/^[0-9A-Fa-f]{6}$/.test(classPart)) {  
+                             color = `#${classPart}`;
+                        } else {
+                            color = classPart; 
+                        }
+                        // In testing: put a console.log() check for color here, it involves the color variable
+                        if (classPlace.includes("bg")) {
+                            $(this).css("background-color", color);
+                        } else if (classPlace.includes("fg")) {
+                            $(this).css("color", color);
+                        }
+                    } else if (classPlace.includes("fs")) {
+                        var classPart = classPlace.substring(3);
+                        // In testing: put a console.log() check for classPart here, it involves the classPart variable
+                        $(this).css("font-size", classPart);
+                    } else if (classPlace.includes("fw")) {
+                        var classPart = classPlace.substring(3);
+                        // In testing: put a console.log() check for classPart here, it involves the classPart variable
+                        $(this).css("font-weight", classPart);
                     }
-                } else {
-                    console.error("[amount] in {NumberChanger, fontWeight} is not a number");
                 }
-            });
-            $(classGet).each(function() {
-                regex = new RegExp("^" + className + "/(\d+(\.\d+)?)(px|em|rem|%|vw|vh|pt|cm|mm|in)$/");
-                amount = classGet.match(regex);
-                console.log("Amount is " + amount);
-                if (amount) {
-                    var fontSize = amount[0];
-                    console.log("font size is " + fontSize);
-                    if (/^(\d+(\.\d+)?)(px|em|rem|%|vw|vh|pt|cm|mm|in)$/.test(fontSize)) {
-                        console.log("Test of Font Size is successful");
-                        $(this).css(cssName, fontSize);
-                    } else {
-                        console.error("[fontSize] returned as " + fontSize);
-                    }
-                } else {
-                    console.error("[amount] in {NumberChanger, fontSize} is not a valid option");
-                }
-            });
+            }
         } else {
-            $(classBack).each(function() {
-                var regex = /^\d+$/;
-                var amount = classGet.match(regex);
-                console.log("Amount is " + amount);
-                if (!isNaN(amount)) {
-                    var fontWeight = amount[0];
-                    console.log("font weight is " + fontWeight);
-                    if (/^\d+$/.test(fontWeight)) {
-                        console.log("Font weight test is successful");
-                        $(this).css(cssName, fontWeight);
-                    } else {
-                        console.error("[fontWeight] returned as " + fontWeight)
-                    }
-                } else {
-                    console.error("[amount] in {NumberChanger, fontWeight} is not a number");
+            /*
+                Figure out how to have it where the parts where it doesn't function properly will end up doing backup code
+            */
+            // In testing: put a console.log() check for backUp being true here, it doesn't involve variables
+            var classError;
+            const jqueryError = new Error("[jQuery] failed to find the class");
+            if (cssName === "background-color" || cssName === "color") {
+                TextBackup("color");
+                if (cssName === "background-color") {
+                    classError = new Error("[bg] jquery failed to find the [bg] class, using backup [bg] code");
+                } else if (cssName === "color") {       
+                    classError = new Error("[fg] jquery failed to find the [fg] class, using backup [fg] code");
                 }
-            });
-            $(classBack).each(function() {
-                regex = /^d+(\.\d+)?(px|em|rem|%|vw|vh|pt|cm|mm|in)$/;
-                amount = classGet.match(regex);
-                console.log("Amount is " + amount);
-                if (amount) {
-                    var fontSize = amount[0];
-                    console.log("font size is " + fontSize);
-                    if (/^(\d+(\.\d+)?)(px|em|rem|%|vw|vh|pt|cm|mm|in)$/.test(fontSize)) {
-                        console.log("Test of Font Size is successful");
-                        $(this).css(cssName, fontSize);
-                    } else {
-                        console.error("[fontSize] returned as " + fontSize);
-                    }
-                } else {
-                    console.error("[amount] in {NumberChanger, fontSize} is not a valid option");
+            } else if (cssName === "font-weight") {
+                TextBackup("other");
+                if (cssName === "font-weight") {
+                    classError = new Error("[fw] jquery failed to find the [fw] class, using backup [fw] code");
                 }
-            });
+            } else if (cssName === "font-size") {          
+                TextBackup("other");
+                if (cssName === "font-size") {
+                    classError = new Error("[fs] jquery failed to find the [fs] class, using backup [fs] code");
+                }
+            }
+            console.error(jqueryError + "\n" + classError);
         }
-    } else if (backUp == true) {
-        TextBackup();
-        if (classGet == undefined) {
-            console.error("[classGet] in {NumberChanger} is undefined");
-        }
-    }
+    });
+    return classLook;
 }
 export function TextFunction() {
-    ColorChanger("bg", "background-color");
-    ColorChanger("fg", "color");
-    NumberChanger("fw", "font-weight");
-    NumberChanger("sz", "font-size");
+    TextOptions("bg", "background-color");
+    TextOptions("fg", "color");
+    TextOptions("fw", "font-weight");
+    TextOptions("fs", "font-size");
 }
