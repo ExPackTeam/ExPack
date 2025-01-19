5778634bcb299439301af59d80f468cc43975fc7
ruby_rose <46199301+MicroRay620@users.noreply.github.com>
2025-01-19 15:07:30 -0500

    modified, attempting to get modals to affect only one modal at a time

--- src/js/modals.js
+++ src/js/modals.js
@@ -1,54 +1,32 @@
 import $ from "jquery";
-// Rewrite Count: 1
+// Rewrite Count: 2
 // Code works, need to figure out how to have it only affect one modal at a time
 export function ModalFunction() {
-    const classLook = $("body").find("[data-popup='modal']").each(function () {
-        const closeButtonCheck = $(".close").length > 0;
-        const modalButtonCheck = $(".modal-btn").length > 0;
-        const modalContentCheck = $(".modal-cnt").length > 0;
-        const modalAlertCheck = $("[data-modal='type-alert']").length > 0;
-        console.log(`{CHECK} [modalButtonCheck] is ${modalButtonCheck} with type of ${typeof modalButtonCheck} \n[closeCheck] is ${closeButtonCheck} with type of ${typeof closeButtonCheck} \n[modal-ctn] is ${modalContentCheck} with type of ${typeof modalContentCheck} \n[modalAlertCheck] is ${modalAlertCheck} with type of ${typeof modalAlertCheck}`);
-        var modalContent = $(".modal-cnt").html();
-        console.log(`{CHECK} [modalContent] is ${modalContent}`);
-        if (modalButtonCheck) {
-            $(".modal-ctn").hide();
-            console.log("{STATUS} [modalContent] hidden");
-            $(".close").hide();
-            console.log("{STATUS} [closeButton] hidden");
-        } else {
-            $(".modal-ctn").show();
-            console.log("{STATUS} [modalContent] shown");
-            $(".close").show();
-            console.log("{STATUS} [closeButton] shown");
+    const modalButton = $("*").hasClass("modal-btn"); // Boolean for modal button
+    console.log(`modal-btn count is ${$(".modal-btn").length}`);
+    const modalContentCheck = $("*").hasClass("modal-cnt"); // Boolean for modal content
+    const closeClass = $("*").hasClass("close"); // Boolean for close
+    // Change all the classes with .length to $("*").hasClass("className");
+    // 
+    const modalAlert = $("[data-modal*='alert']").length > 0;
+    console.log(`{CHECK} [modalBtn] is ${modalButton} \n [modalCnt] is ${modalContentCheck} \n [closeClass] is ${closeClass}`);
+    if (modalContentCheck) {
+        console.log("{STATUS} modal content is true");
+        var modalSize = $("[data-popup='modal']").length;
+        console.log(`{CHECK} [modalSize] is ${modalSize}`);
+        console.log(`{CHECK} [modalSize] is ${modalSize}`);
+        for (var i = modalSize + 1; i > modalSize; i--) {
+            if (modalButton) {
+                console.log("{STATUS} modalButton is still valid");
+                // Add the button shifting option once the single instance is working
+                $(".modal-cnt").hide();
+                $(".close").hide();
+                $(".modal-btn").on("click", function () {
+                    console.log("{STATUS} button was clicked");
+                    $(".modal-cnt").show();
+                    $(".close").show();
+                })
+            }
         }
-        if (modalAlertCheck) {
-            $(".modal-ctn").show();
-            $(".close").hide();
-            alert(modalContent);
-        }
-        /*
-            # Attempts
-            - $(this).closest(CLASS)
-            ~> Result: fail
-            - const targetModal = $(this).data('modal'); +  $(`${targetModal} CLASS`).show()
-            ~> Result: failed
-            - $(event.target).children()
-            ~> Result: failed
-            // Doing $("[data-popup='modal']").children() results in both modals being affected
-            - $("CLASS").parent().show()
-            ~> Result: failed
-        */
-        $(".modal-btn").on("click", function () {
-            $(this).siblings(".modal-ctn").show();
-            $(".close").show();
-            $(this).hide();
-            // Using $(this) works properly, but need a way to do that in the button 
-        });
-        $(".close").on("click", function () {
-            $(".modal-ctn").hide();
-            $(this).hide();
-            $(".modal-btn").show();
-        });
-    });
-    return classLook;
+    }
 }
