b750d7364ebbefc6dd20212ca9bead2aca74179e
ruby_rose <46199301+MicroRay620@users.noreply.github.com>
2025-01-20 15:39:25 -0500

    modified, reworking copy clip

--- src/js/copy_clip.js
+++ src/js/copy_clip.js
@@ -5,22 +5,18 @@ import { CopyBackup } from "./backup";
     - Clip [data-clip]
         - Text [data-clip="text"]
         // data-clip="text" defines the whole area that will be added to the clipboard
-        - Select [data-clip="select"]
-        // data-clip="select" defined the text that will be added to the clipboard
-        // Select may change to a class, if it does make it called copy-sel
     Classes:
     - Copy Button [class="copy-btn"]
     // copy-btn is what will copy the text to the clip-board
+    - Copy Sel [class="copy-sel"]
     Functions:
     - Look for the copying section [data-clip="text"]
-    - Get all text in the section that is copyable [data-clip="select"]
+    - Get all text in the section that is copyable [class="copy-sel"]
     // If [data-clip="select"] is made into a class, it will be [class="copy-sel"]
     - When [class="copy-btn"] is clicked, the text is added to the local clip-board
 */
-function GetCopy() {
-    const copyArea = $(this).attr("data", "text");
-    console.log("[copyArea] is " + copyArea);
-}
 export function CopyClip() { 
-    GetCopy();
+    if ($("*").attr("data-clip", "text")) {
+    }
+    console.log(`{CHECK} [clipText] is ${clipText}`);
 }
