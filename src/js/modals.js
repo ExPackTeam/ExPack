d52741271ec752ad59b6be74e7173fba8f363925
ruby_rose <46199301+MicroRay620@users.noreply.github.com>
2025-01-20 15:39:38 -0500

    modified, completed the modal function

--- src/js/modals.js
+++ src/js/modals.js
@@ -1,32 +1,24 @@
 import $ from "jquery";
-// Rewrite Count: 2
+// Rewrite Count: 3
 // Code works, need to figure out how to have it only affect one modal at a time
+// Will figure out how to add the backup code at a later date
 export function ModalFunction() {
-    const modalButton = $("*").hasClass("modal-btn"); // Boolean for modal button
-    console.log(`modal-btn count is ${$(".modal-btn").length}`);
-    const modalContentCheck = $("*").hasClass("modal-cnt"); // Boolean for modal content
-    const closeClass = $("*").hasClass("close"); // Boolean for close
-    // Change all the classes with .length to $("*").hasClass("className");
-    // 
-    const modalAlert = $("[data-modal*='alert']").length > 0;
-    console.log(`{CHECK} [modalBtn] is ${modalButton} \n [modalCnt] is ${modalContentCheck} \n [closeClass] is ${closeClass}`);
-    if (modalContentCheck) {
-        console.log("{STATUS} modal content is true");
-        var modalSize = $("[data-popup='modal']").length;
-        console.log(`{CHECK} [modalSize] is ${modalSize}`);
-        console.log(`{CHECK} [modalSize] is ${modalSize}`);
-        for (var i = modalSize + 1; i > modalSize; i--) {
-            if (modalButton) {
-                console.log("{STATUS} modalButton is still valid");
-                // Add the button shifting option once the single instance is working
-                $(".modal-cnt").hide();
-                $(".close").hide();
-                $(".modal-btn").on("click", function () {
-                    console.log("{STATUS} button was clicked");
-                    $(".modal-cnt").show();
-                    $(".close").show();
-                })
-            }
-        }
+    if ($("*").hasClass("modal-hid")) {
+        $(".modal-cnt").hide();
+        $(".close").hide();
+    } else if ($("*").hasClass("modal-shn")) {
+        $(".modal-cnt").show();
+        $(".close").show();
+    } else {
+        $(".modal-cnt").hide();
+        $(".close").hide();
     }
+    $(".modal-btn").on("click", function() {
+        $(this).closest("[data-popup='modal']").find(".modal-cnt").show();
+        $(this).closest("[data-popup='modal']").find(".close").show();
+    });
+    $(".close").on("click", function() {
+        $(this).closest("[data-popup='modal']").find(".modal-cnt").hide();
+        $(this).closest("[data-popup='modal']").find(".close").hide();
+    });
 }
