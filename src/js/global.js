0222f3313491caa4c7eb62d34d9a4fa4be2ca0c9
ruby_rose <46199301+MicroRay620@users.noreply.github.com>
2025-01-20 15:38:58 -0500

    modified, added copy_clip

--- src/js/global.js
+++ src/js/global.js
@@ -3,12 +3,14 @@ import { TextFunction } from "./text_changes.js";
 import { PageFunction } from "./page_options.js";
 import { LinkExport } from "./link_options.js";
 import { ModalFunction } from "./modals"; // Modals are causing PageFunction to break, reason unknown
+import { CopyClip } from "./copy_clip.js";
 function ExpackExport() {
     $(function() {
         TextFunction();
         PageFunction();
         LinkExport();
         ModalFunction();
+        CopyClip();
     });
 }
 export default ExpackExport();
