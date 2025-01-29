a3b07a26f4c18e408096e54916cb9e9a5fa0988f
ruby_rose <46199301+MicroRay620@users.noreply.github.com>
2025-01-29 08:01:34 -0500

    modified, removed link_options

--- src/js/global.js
+++ src/js/global.js
@@ -1,7 +1,6 @@
 import $ from "jquery";
 import { TextFunction } from "./text_changes.js";
 import { PageFunction } from "./page_options.js";
-import { LinkExport } from "./link_options.js";
 import { ModalFunction } from "./modals"; // Modals are causing PageFunction to break, reason unknown
 import { CopyClip } from "./copy_clip.js";
 function ExpackExport() {
