28e34dddacae36ef3f84244c368fbc3382f8940f
MicroRay620 <microray620@gmail.com>
2025-09-01 20:04:09 -0400

    redoing page_options, wip

--- src/js/page_options.js
+++ src/js/page_options.js
@@ -2,10 +2,8 @@ import $ from "jquery";
 import { PageBackup } from "./backup";
 // Retype Count: ~5 // This is the amount I have re-written this code FROM scratch
 // All `console.log()`s are for debugging, they are going to removed once it's ready for the master branch
-function PageClasses() {
-      
+function PageOptions() {
 }
 export function PageFunction() {
-    PageClasses();
-    PageDatas();
+    PageOptions();
 }
