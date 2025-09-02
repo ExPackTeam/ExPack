0357b5d67c4c403ca241a402c0c3e09bc2314365
MicroRay620 <microray620@gmail.com>
2025-09-01 20:05:40 -0400

    expanded the source, runs better now

--- rollup.config.js
+++ rollup.config.js
@@ -4,92 +4,114 @@ import { babel } from "@rollup/plugin-babel";
 import terser from "@rollup/plugin-terser";
 import postcss from 'rollup-plugin-postcss';
 import postcssAttributeCaseInsensitive from "postcss-attribute-case-insensitive";
-
-// Shared plugins configuration
-const SharedPlugins = [
-  resolve(),
-  commonjs(),
-  babel({
-    exclude: "node_modules/**",
-    presets: [["@babel/preset-env", {
-      targets: "defaults, not ie 11",
-      bugfixes: true
-    }]],
-    babelHelpers: "bundled",
-  }),
-  postcss({
-    extract: true,
-    modules: true,
-    minimize: false,
-    sourcemap: false,
-    plugins: [postcssAttributeCaseInsensitive()],
-  }),
-];
-
-const MinizedSharedPlugins = [
+const basePlugin = {
+  plugins: [
     resolve(),
     commonjs(),
     babel({
-        exlude: "node_modules/**",
-        presets: [["@babel/present-env", {
-            targets: "defaults, not ie 11",
-            bugfixes: true
-        }]],
-        babelHelpers: "bundled"
+      exclude: "node_modules/**",
+      presets: ["@babel/preset-env"],
+      babelHelpers: "bundled",
     }),
     postcss({
-        extract: true,
-        modules: true,
-        minized: true,
-        sourcemap: false,
-        plugins: [postcssAttributeCaseInsensitive()], 
-    })
-]
-
-// Base configuration
-const BaseConfig = {
-  plugins: SharedPlugins,
+      extract: true, // Extract CSS into a separate file
+      modules: true, // Set to true if you want CSS modules
+      minimize: false, // Change to false to prevent minification in the main CSS
+      sourcemap: false, // Generate source maps
+      plugins: [
+        postcssAttributeCaseInsensitive(),
+      ]
+    }),
+  ],
 };
 
-// JavaScript configuration
-const JsConfig = {
-  ...BaseConfig,
+const basejsConfig = {
   input: "src/js/global.js",
+  ...basePlugin,
+};
+
+const baseCSSConfig = {
+  input: "src/scss/global.scss",
+  ...basePlugin,
+};
+
+const jsConfig = {
   output: {
     file: "dist/js/expack.esm.js",
     format: "esm",
     name: "ExPack",
-    sourcemap: false,
+    sourcemap: false, // Generate source maps
   },
   external: ['jquery'],
   plugins: [
-    ...SharedPlugins,
     terser({
       mangle: {
         reserved: ['$', 'jquery'],
-      },
-      format: {
-        comments: false,
-      },
-      compress: {
-        drop_console: false, // Set to true for production
       },
     }),
   ],
 };
 
-const JsMiniConfig = {
-    ...
-}
-
-// CSS configuration
-const CssConfig = {
-  ...BaseConfig,
-  input: "src/scss/global.scss",
+const jsMiniConfig = {
   output: {
-    file: "dist/css/expack.esm.css", // Explicit output for CSS
-    format: "esm", // CSS doesn't need module format but this prevents warnings
+    file: "dist/js/expack.min.js",
+    format: "iife",
+    name: "ExPack",
+    sourcemap: false,
   },
 };
 
-export default [jsConfig, cssConfig];
+const cssConfig = {
+  output: {
+    file: "dist/css/expack.esm.css",
+    format: "esm",
+    name: "Expack",
+    sourcemap: false,
+  },
+};
+
+const cssMiniConfig = {
+  output: {
+    file: "dist/css/expack.min.css",
+    format: "iife",
+    name: "Expack",
+    sourcemap: false,
+  },
+};
+export default [
+  {
+    ...basejsConfig,
+    ...jsConfig,
+  },
+  {
+    ...basejsConfig,
+    plugins: [
+      ...basePlugin.plugins,
+      terser(), // Apply Terser plugin only to the minimized output
+    ],
+    ...jsMiniConfig,
+  },
+  {
+    ...baseCSSConfig,
+    ...cssConfig,
+  },
+  {
+    ...baseCSSConfig,
+    plugins: [
+      resolve(),
+      commonjs(),
+      babel({
+        exclude: "node_modules/**",
+        presets: ["@babel/preset-env"],
+        babelHelpers: "bundled",
+      }),
+      postcss({
+        extract: true,
+        modules: false,
+        minimize: true, // Minify only in the minified output
+        sourceMap: false, // No source maps for minified output
+      }),
+    ],
+    ...cssMiniConfig,
+  }
+];
